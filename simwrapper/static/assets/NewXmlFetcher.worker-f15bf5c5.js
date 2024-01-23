ae834860ee2d52bdf1d46b86e9cb1481a6e0a1b1
Billy Charlton <billy@okbecause.com>
2024-01-23 17:30:14 +1100

    beam build

---
+++ simwrapper/static/assets/NewXmlFetcher.worker-f15bf5c5.js
@@ -0,0 +1,2 @@
+function r(){return new Worker("/assets/NewXmlFetcher.worker-000afa4f.js")}export{r as W};
+//# sourceMappingURL=NewXmlFetcher.worker-f15bf5c5.js.map
---
+++ simwrapper/static/assets/RoadNetworkLoader.worker-0207bb7c.js
@@ -0,0 +1,2 @@
+function r(){return new Worker("/assets/RoadNetworkLoader.worker-d37c9460.js")}export{r as W};
+//# sourceMappingURL=RoadNetworkLoader.worker-0207bb7c.js.map
